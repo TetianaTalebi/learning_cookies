f537255be18862d7df7d40e8ba84d3aacbbe48c0
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-28 21:50:26 -0400

    Add app.get(/setname... into index.js

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/greet', (req, res) => {
     res.send('Hey there!');
 })
 
+app.get('/setname', (req, res) => {
+
+    // Cookies are send back as a part of a response
+    res.cookie('name', 'Rosa');
+    res.cookie('animal', 'cat');
+
+    // A response itself
+    res.send('Sending a cookie!');
+})
+
 // Starting a web server and listening to the connections on the port 3000
 app.listen(3000, () => {
     console.log('Serving on port 3000 !');
