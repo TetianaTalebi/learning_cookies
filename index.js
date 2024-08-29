5b8eec0d921358b047267ad606db71915ff6e8b3
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-28 21:43:40 -0400

    Add app.get(/greet... into index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/greet', (req, res) => {
+    res.send('Hey there!');
+})
+
 // Starting a web server and listening to the connections on the port 3000
 app.listen(3000, () => {
     console.log('Serving on port 3000 !');
