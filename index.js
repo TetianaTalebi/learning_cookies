84bfb0fd7ce0915aafb5743e002aa17a4a46dcb1
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-28 21:39:25 -0400

    Add some comments into index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// Creating an Express application
 const express = require('express');
 const app = express();
 
+// Starting a web server and listening to the connections on the port 3000
 app.listen(3000, () => {
     console.log('Serving on port 3000 !');
 })
