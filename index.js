c9b170a9bc8be78a257f5835825d015841547b61
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-30 21:30:14 -0400

    Add app.get(/getsignedcookie... into index.js

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.get('/setname', (req, res) => {
     res.send('Sending a cookie!');
 })
 
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'apple', {signed: true});
+    res.cookie('vegetable', 'potato', {signed: true});
+    res.send('Signed fruit and vegetable cookies!');
+})
+
 // Starting a web server and listening to the connections on the port 3000
 app.listen(3000, () => {
     console.log('Serving on port 3000 !');
